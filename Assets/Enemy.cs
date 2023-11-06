e8bb8848950cac586e3cf8fa43c8a802128e7671
Gonztark <eduarjoseg12@gmail.com>
2023-11-05 23:38:52 -0400

    Juego de plataformas inicio

---
+++ Assets/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int maxHealth = 100;
+    int currentHealth;
+    public Animator animator;
+	
+	// Update is called once per frame
+	void Start () {
+        currentHealth = maxHealth;
+		
+	}
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        animator.SetTrigger("Hurt");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died");
+        animator.SetBool("IsDead", true);
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        foreach (Collider2D collider in colliders)
+        {
+            collider.enabled = false;
+        }
+
+        // Access the Rigidbody2D component
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        // If you want the enemy to stop all physics interaction (freeze in place)
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+
+        this.enabled = false;
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public Animator animator;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+    private bool jumpHasPeaked = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+            StartCoroutine(JumpPeakDelay());
+
+        }
+    
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            
+            crouch = true;
+            animator.SetBool("IsCrouching", true);
+
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            animator.SetBool("IsCrouching", false);
+        }
+    }
+
+    IEnumerator JumpPeakDelay()
+    {
+        // Wait for the expected peak time of the jump
+        yield return new WaitForSeconds(0.5f); // Adjust the time to the peak of your jump
+        jumpHasPeaked = true;
+    }
+
+    public void OnLanding()
+    {
+        Debug.Log("LAAAAANDING");
+        if (jumpHasPeaked)
+        {
+            animator.SetBool("IsJumping", false);
+            jumpHasPeaked = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+
+
+    }
+}
